7631091fe814e855aefd5283fce66088ee0022c3
Logiqx <software@mikeg.me.uk>
2022-10-10 16:33:11 +0100

    Implement logging to filer from server

--- bin/env.sh
+++ bin/env.sh
@@ -10,8 +10,8 @@ run_py_script()
 {
   docker run -it --rm \
          --dns 192.168.0.254 \
-         --mount type=bind,src=$PROJ_DIR/events,dst=$WORK_DIR/events,readonly \
-         --mount type=bind,src=$PROJ_DIR/config,dst=$WORK_DIR/config,readonly \
+         --mount type=bind,src=$PROJ_DIR/events,dst=$WORK_DIR/events \
+         --mount type=bind,src=$PROJ_DIR/config,dst=$WORK_DIR/config \
          --mount type=bind,src=$PROJ_DIR/docs,dst=$WORK_DIR/docs \
          $IMAGE_NAME:${IMAGE_TAG:-latest} python/$1
 }
